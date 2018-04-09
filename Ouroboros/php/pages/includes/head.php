798052c89087a58b53c205af0ca140893bb128d5
Justin Baker <jusitn.baker@easternstudent.ca>
2018-04-09 09:31:28 -0300

    Added ouroboros website test code

---
+++ Ouroboros/php/pages/includes/head.php
@@ -0,0 +1,4 @@
+<title><?php 
+        session_start();echo($title); ?></title>
+<meta name"description" "<?php echo($metaTags); ?>"><!-- note that this code is wrong -->
+<link rel="stylesheet" type="text/css" href="/Ouroboros/css/main.css">
